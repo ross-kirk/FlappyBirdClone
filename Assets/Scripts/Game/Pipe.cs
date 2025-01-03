27810fcab321fe3f9fdce544c545a8134629b97b
Ross Kirk <rosskirk365@gmail.com>
2025-01-03 17:45:24 +0000

    Pipe controller updates

--- Assets/Scripts/Game/Pipe.cs
+++ Assets/Scripts/Game/Pipe.cs
@@ -1,14 +1,22 @@
+using System;
 using UnityEngine;
 
 namespace Game
 {
     public class Pipe : MonoBehaviour
     {
+        public event Action<Pipe> OnPipeDestroyed;
         public float Speed { get; private set; }
 
         public void SetSpeed(float speed)
         {
             Speed = speed;
+        }
+
+        public void DestroySelf()
+        {
+            Destroy(gameObject);
+            OnPipeDestroyed?.Invoke(this);
         }
 
         private void FixedUpdate()
--- Assets/Scripts/Game/PipeController.cs
+++ Assets/Scripts/Game/PipeController.cs
@@ -1,5 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Core;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Game
 {
@@ -7,10 +11,35 @@ namespace Game
     {
         [SerializeField] private float pipeSpeed;
         [SerializeField] private float pipeSpawnDelay;
-        
-        private Pipe[] pipes;
+        [SerializeField] private GameObject pipePrefab;
 
-        private StateMachine gameStateMachine = new StateMachine();
+        private bool shouldSpawn;
+        private List<Pipe> pipes;
+        private Vector3 screenBounds;
+
+        private void Start()
+        {
+            screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+            InvokeRepeating("SpawnPipe", 1f, pipeSpawnDelay);
+        }
+
+        public void StartSpawner()
+        {
+            shouldSpawn = true;
+        }
+
+        public void StopSpawner()
+        {
+            shouldSpawn = false;
+        }
+
+        public void StartPipes()
+        {
+            foreach (var pipe in pipes)
+            {
+                pipe.SetSpeed(pipeSpeed);
+            }
+        }
 
         public void StopPipes()
         {
@@ -18,6 +47,37 @@ namespace Game
             {
                 pipe.SetSpeed(0);
             }
+        }
+
+        public void RemoveAllPipes()
+        {
+            foreach (var pipe in pipes)
+            {
+                pipe.DestroySelf();
+            }
+        }
+
+        private void RemovePipe(Pipe pipeToRemove)
+        {
+            foreach (var pipe in pipes.ToList().Where(pipe => pipe == pipeToRemove))
+            {
+                pipes.Remove(pipe);
+                pipeToRemove.OnPipeDestroyed -= RemovePipe;
+            }
+        }
+
+        private void SpawnPipe()
+        {
+            if (!shouldSpawn)
+            {
+                return;
+            }
+            
+            var yRandom = Random.Range(0, screenBounds.y);
+            var pipe = Instantiate(pipePrefab, new Vector3(screenBounds.x + 30, yRandom, screenBounds.z),
+                Quaternion.identity).GetComponent<Pipe>();
+            pipe.OnPipeDestroyed += RemovePipe;
+            pipes.Add(pipe);
         }
     }
 }
